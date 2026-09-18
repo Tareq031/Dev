f7fb6e4fdabcf7cf0e5449130b2e682917203854
Tareq031 <tareq.cse031@gmail.com>
2026-09-18 15:47:32 +0600

    Initialize React TypeScript project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+
+
+  return (
+    <>
+      test
+      <button className="btn btn-warning">Warning</button>
+<button className="btn btn-error">Error</button>
+    </>
+  )
+}
+
+export default App
