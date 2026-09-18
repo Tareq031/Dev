29ac1ec8004a24256524dc6876d8630e5721b826
Tareq031 <tareq.cse031@gmail.com>
2026-09-18 17:35:40 +0600

    completed navbar section

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -21,6 +21,15 @@ const Nav = () => {
           <li>About</li>
           <li>Contact</li>
         </ul>
+        <div className="flex items-center gap-3">
+          <button className="rounded-md border  text-red-400  border-gray-600 px-4 py-2">
+            Sign In
+          </button>
+
+          <button className="rounded-md bg-primary px-4 py-2 text-white">
+            Sign Up
+          </button>
+        </div>
 
        
         
